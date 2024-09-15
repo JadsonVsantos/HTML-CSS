44bbeed7342410123fe43b1a3a229255e9f315cc
Jadson VSantos <jadsonvenancio1983@gmail.com>
2024-09-15 15:58:54 -0300

    Código atualizado

--- Projetos/Portfolio de apresentação/assets/js/script.js
+++ Projetos/Portfolio de apresentação/assets/js/script.js
@@ -1,20 +1,30 @@
 document.getElementById("form-contato").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const mensagem = document.getElementById("mensagem").value;
-  
-    // Aqui, você pode integrar com um serviço de envio de e-mail ou backend
-    const mensagemEnviada = `Obrigado, ${nome}. Sua mensagem foi enviada com sucesso!`;
-  
-    document.getElementById("mensagem-enviada").textContent = mensagemEnviada;
-    document.getElementById("mensagem-enviada").textContent = mensagemEnviada;
+  event.preventDefault();
 
-    // Limpar os campos do formulário após o envio
-    document.getElementById("nome").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("mensagem").value = "";
-  });
-  
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const mensagem = document.getElementById("mensagem").value;
+
+  // Configurar os parâmetros de envio de e-mail
+  const params = {
+    from_name: nome,
+    email_id: email,
+    message: mensagem,
+  };
+
+  emailjs.send("service_sc63lhm", "template_yqsbwek", params, "AOPNORsK6Z0MBdfTi")
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+      document.getElementById("mensagem-enviada").textContent = `Obrigado, ${nome}. Sua mensagem foi enviada com sucesso!`;
+
+      // Limpar o formulário após o envio
+      document.getElementById("nome").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("mensagem").value = "";
+    }, function(error) {
+      console.log('FAILED...', error);
+      document.getElementById("mensagem-enviada").textContent = "Ocorreu um erro ao enviar sua mensagem. Tente novamente mais tarde.";
+    });
+});
+
   
