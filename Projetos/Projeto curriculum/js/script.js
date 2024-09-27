cd49c3bcaba200946b1f80efa9ff40f4a1a258ee
Jadson VSantos <jadsonvenancio1983@gmail.com>
2024-09-26 22:32:36 -0300

    Projetos atualizados

---
+++ Projetos/Projeto curriculum/js/script.js
@@ -0,0 +1,6 @@
+// Add event listeners to social media links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', () => {
+        // Add animation or other effects here
+    });
+});
